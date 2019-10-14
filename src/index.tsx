f7561efc6bd039495970cd1950471a14f23f7acf
Marcos Choque <marcos.choque@orbis.com.pe>
2019-10-13 23:06:17 -0500

    init

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => <div>HolaMundo</div>;
+const root = document.getElementById('root');
+ReactDOM.render(<App/>, root);
